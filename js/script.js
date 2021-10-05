d85b762313572ce77b303264a974cb7a092d9f18
vendymasarova <vendymasarova@seznam.cz>
2021-10-05 12:38:01 +0200

    project is done without flip animation

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const countdown = () => {
+  const countDate = new Date("October 27, 2021 10:59:00").getTime();
+
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.querySelector(".day").innerHTML = textDay;
+  document.querySelector(".hour").innerHTML = textHour;
+  document.querySelector(".minute").innerHTML = textMinute;
+  document.querySelector(".second").innerHTML = textSecond;
+};
+setInterval(countdown, 100);
